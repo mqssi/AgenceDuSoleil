d46e41af8a032993fab8f90d64825e02ec79238a
mqssi <43760122+mqssi@users.noreply.github.com>
2020-04-28 12:09:27 +0200

    ajout dl client lourd

--- src/Controller/DocuController.php
+++ src/Controller/DocuController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DocuController extends AbstractController
@@ -27,10 +29,21 @@ class DocuController extends AbstractController
         ]);
 
 
+
     }
 
 
+    /**
+     * @Route("/download", name="download_file")
+    **/
+     public function downloadFile()
+    {
+        $response = new BinaryFileResponse('../EasyStock.rar');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT,'EasyStock.rar');
+        return $response;
 
+
+    }
 
 
 }
